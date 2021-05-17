9dea2a30f29e933f80d8eb3647d4675a59d33eda
sunny.ayyl <37855944+sunny0531@users.noreply.github.com>
2021-05-17 22:34:42 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,4 +1,4 @@
 with open("/home/runner/work/RPMTW-website/RPMTW-website/build/web/index.html","r") as f:
-  text=f.read().replace('<base href="/">','<base href="/docs">'))
+  text=f.read().replace('<base href="/">','<base href="/docs">')
 with open("/home/runner/work/RPMTW-website/RPMTW-website/build/web/index.html","w") as ff:
   ff.write(text)
