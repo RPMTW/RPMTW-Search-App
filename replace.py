b9b756f3d9cda1c5569c9c1e1c9d5f0d963dc64c
sunny.ayyl <37855944+sunny0531@users.noreply.github.com>
2021-05-17 22:32:13 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,2 +1,4 @@
-with open("/home/runner/work/RPMTW-website/RPMTW-website/build/web/index.html","w+") as f:
-  f.write(f.read().replace('<base href="/">','<base href="/docs">'))
+with open("/home/runner/work/RPMTW-website/RPMTW-website/build/web/index.html","r") as f:
+  text=f.read().replace('<base href="/">','<base href="/docs">'))
+with open("/home/runner/work/RPMTW-website/RPMTW-website/build/web/index.html","w") as ff:
+  ff.write(text)
