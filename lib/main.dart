eca589e7f5bb13aa81e8e353d1e112e02db9656c
sunny.ayyl <sunny.ayyl@gmail.com>
2021-05-18 17:27:29 +0800

    A small website update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:http/http.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +16,14 @@ class MyApp extends StatefulWidget {
 
 class _MyApp extends State<MyApp> {
   var title = "RPMTW";
+  openURL() async {
+    const url = 'https://github.com/RPMTW/';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   get_mod_list() async {
     // make GET request
@@ -23,8 +34,8 @@ class _MyApp extends State<MyApp> {
     final url2 = Uri.parse(
         'https://raw.githubusercontent.com/sunny0531/RPMTW-website-data/main/progress.txt');
     Response response2 = await get(url2);
-    Map<String, dynamic> user =await jsonDecode(response2.body);
-    return [respond_,user];
+    Map<String, dynamic> user = await jsonDecode(response2.body);
+    return [respond_, user];
   }
 
   final TextEditingController input_controller = TextEditingController();
@@ -35,40 +46,85 @@ class _MyApp extends State<MyApp> {
         title: title,
         home: Scaffold(
           body: Center(
-              child: ListView(children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'Mod name'),
-              controller: input_controller,
-              onChanged: (text) {
-                setState(() {
-                });;
-              },
-            ),
-            FutureBuilder(
-              future: get_mod_list(),
-              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                if (snapshot.hasData) {
-                  if (snapshot.data[0].contains(input_controller.text)) {
-                    var data_=snapshot.data[1][input_controller.text];
-                    if (data_==null){
-                      data_=0;
-                    }
-                    return Text("模組已經在資料庫內了，目前翻譯進度 "+data_.toString()+"% ");
-                  } else if(input_controller.text.isNotEmpty) {
-                    return Text(
-                        "模組不在資料庫內，歡迎協助翻譯，詳請請查看: https://www.rpmtw.ga 或者RPMTW官方Discord伺服器");
-                  }else{
-                    return Text(
-                        "請輸入模組名稱");
-                  }
-                } else {
-                  return Center(child: CircularProgressIndicator());
-                }
-              },
-            ),
-                Text("Update ever 30 minutes")
-          ])),
+              child: Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  child: ListView(children: <Widget>[
+                    SizedBox(
+                      width: 10,
+                      height: 10,
+                    ),
+                    InkWell(
+                      onTap: openURL,
+                      child: Ink.image(
+                        image: AssetImage('assets/icon.png'),
+                        height: 170,
+                        width: 170,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 25,
+                      height: 25,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), hintText: 'Mod name'),
+                      controller: input_controller,
+                      onChanged: (text) {
+                        setState(() {});
+                        ;
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                      width: 2,
+                    ),
+                    FutureBuilder(
+                      future: get_mod_list(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<dynamic> snapshot) {
+                        if (snapshot.hasData) {
+                          if (snapshot.data[0]
+                              .contains(input_controller.text)) {
+                            var data_ = snapshot.data[1][input_controller.text];
+                            if (data_ == null) {
+                              data_ = 0;
+                            }
+                            return Text(
+                              "模組已經在資料庫內了，目前翻譯進度 " + data_.toString() + "% ",
+                              textAlign: TextAlign.center,
+                              style: DefaultTextStyle.of(context)
+                                  .style
+                                  .apply(fontSizeFactor: 1.3),
+                            );
+                          } else if (input_controller.text.isNotEmpty) {
+                            return Text(
+                              "模組不在資料庫內，歡迎協助翻譯，詳請請查看: https://www.rpmtw.ga 或者RPMTW官方Discord伺服器",
+                              textAlign: TextAlign.center,
+                              style: DefaultTextStyle.of(context)
+                                  .style
+                                  .apply(fontSizeFactor: 1.3),
+                            );
+                          } else {
+                            return Text(
+                              "請輸入模組名稱",
+                              textAlign: TextAlign.center,
+                              style: DefaultTextStyle.of(context)
+                                  .style
+                                  .apply(fontSizeFactor: 1.25),
+                            );
+                          }
+                        } else {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      },
+                    ),
+                    Text(
+                      "Update every 30 minutes",
+                      textAlign: TextAlign.center,
+                    )
+                  ]))),
         ));
   }
 }
