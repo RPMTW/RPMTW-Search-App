f0a2930b0bbe75bfc1b073bd2764ce468b592298
sunny.ayyl <sunny.ayyl@gmail.com>
2021-05-19 21:03:02 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -111,7 +111,6 @@ class _MyApp extends State<MyApp> {
                       "每三十分鐘更新資料一次\n"+
                       "此網頁主要由 sunny.ayyl#2932 開發製作，詳情請查看 Github儲存庫。",
                       textAlign: TextAlign.center,
-                      style: DefaultTextStyle.of(context)
                     )
                   ]))),
         ));
