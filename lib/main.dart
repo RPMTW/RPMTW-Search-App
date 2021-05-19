8a0ef05c7056b8dee4ca13105c405ea6c3c9ccaa
菘菘 <rrt467778@gmail.com>
2021-05-19 17:40:17 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -122,9 +122,6 @@ class _MyApp extends State<MyApp> {
                     ),
                     Text(
                       "每三十分鐘更新資料一次",
-                      textAlign: TextAlign.center,
-                    )
-                    Text(
                       "此網頁主要由 sunny.ayyl#2932 開發製作，詳情請查看 Github儲存庫。",
                       textAlign: TextAlign.center,
                     )
